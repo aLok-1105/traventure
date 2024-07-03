64bbe5d17845b6d6fb674fdfac51774386c69742
aLok-1105 <alokranjan11052003@gmail.com>
2024-07-03 13:43:34 +0530

    img-upload-create-post

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,22 +1,79 @@
-import { Box, Button, Grid, Input, TextField } from '@mui/material'
-import React, { useState } from 'react';
+import { Box, Button, CircularProgress, Grid, Input, TextField } from '@mui/material'
+import React, { useEffect, useState } from 'react';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import Container from '@mui/material/Container';
 // import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import axios from 'axios';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
 
 export default function CreatePost() {
       
     const [formData, setFormData] = useState({});
+    const [imgFile, setImgFile] = useState(null);
+    const [imgFileUrl, setImgFileUrl] = useState(null);
+    const [imgUploading, setImgUploading] = useState(false);
+    const [imgUploadError, setImgUploadError] = useState(null);
+    const [imgUploadProgress, setImgUploadProgress] = useState(null);
+
     const handleChange = ((e)=>{
       setFormData({...formData, [e.target.name]:e.target.value})
     })
+
+
+
+    const handleUpload = async () => {
+      try {
+        if(!imgFile){
+          setImgUploadError("Please Select an Image");
+          setImgUploading(true);
+        }
+        setImgUploadError(null);
+        setImgUploading(true);
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + imgFile.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, imgFile);
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+                setImgUploadProgress(progress.toFixed(0));
+            },
+            (error) => {
+                setImgUploadError(
+                    'Could not upload image (File must be less than 5MB)'
+                );
+                setImgUploadProgress(null);
+                setImgFileUrl(null);
+                setImgUploading(false);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setImgFileUrl(downloadURL);
+                    console.log(downloadURL);
+                    setFormData({ ...formData, imageURL: downloadURL });
+                    setImgUploading(false);
+                });
+            }
+        );
+        }
+        catch (error) {
+        setImgUploadError("Image Upload Error");
+        setFormData({ ...formData, imageURL: null });
+      }
+        
+    };
 
     
 
     const handleSubmit =async (e) => {
       e.preventDefault();
+      console.log(formData);
       if(!formData.title || !formData.location || !formData.budget ||  !formData.days  || !formData.description){
         alert("Please fill all the fields")
       }
@@ -85,9 +142,9 @@ export default function CreatePost() {
               onChange={handleChange}
               />
             </Grid>
-            <Grid container item xs={12} spacing={2}>
-              <Grid item xs={8} style={{cursor:"pointer"}}>
-                <Input type="file" name="imageURL" fullWidth  />
+            <Grid container item xs={12} spacing={2} style={{display:"flex", alignItems:"center"}}>
+              <Grid item xs={7} style={{cursor:"pointer"}}>
+                <Input type="file" name="imageURL" accept='image/*' fullWidth onChange={(e)=>setImgFile( e.target.files[0])} />
               </Grid>
               <Grid item xs={4}>
                 <Button
@@ -95,10 +152,30 @@ export default function CreatePost() {
                   variant="contained"
                   startIcon={<CloudUploadIcon />}
                   fullWidth
+                  onClick={handleUpload}
                 >
                   Upload
                 </Button>
+                
               </Grid>
+              <Grid item xs={1} style={{paddingLeft: "3px"}}>
+              {
+                  imgUploadProgress ? (
+                    
+                    <CircularProgressbar
+                      value={imgUploadProgress}
+                      text = {`${imgUploadProgress || 0}%`}
+                      />
+                      
+                  )
+                  :(
+                    <CircularProgressbar
+                      value={imgUploadProgress}
+                      text = {`${imgUploadProgress || 0}%`}
+                      />
+                  )
+                }
+                </Grid>
             </Grid>
             <Grid item xs={12}>
               {/* <ReactQuill theme="snow" style={{ height: '200px', cursor: 'text' }} onChange={(val)=>{setFormData({...formData, description: val})} }/>  */}
@@ -116,6 +193,7 @@ export default function CreatePost() {
                   type = "submit"
                   variant="contained"
                   fullWidth
+                  disabled={imgUploading}
                 >
                   Submit
             </Button>
--- server/routes/post.js
+++ server/routes/post.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.post('/create', validateToken1, async (req, res)=>{
     console.log(req.body);
-    const {title, location, budget, days, description} = req.body.formData;
+    const {title, location, budget, days, description, imageURL} = req.body.formData;
     if(!title || !location || !budget || !days || !description){
         return res.status(422).json({ error: 'Fill Data' });
     }
@@ -17,6 +17,7 @@ router.post('/create', validateToken1, async (req, res)=>{
             budget,
             days,
             description,
+            imageURL: imageURL == null ? 'https://images.pexels.com/photos/620337/pexels-photo-620337.jpeg?cs=srgb&dl=pexels-pripicart-620337.jpg&fm=jpg' : imageURL,
             createdBy: req.user._id,
         });
         res.status(201).json({
